b96f1c94ed9318a6d0a772f20c0f1dc26e803c90
Mirko Maggioni <mirko.maggioni@sintel.net>
2018-05-05 18:54:41 +0200

    RabbitMQ service is added, send and receive methods are implemented

---
+++ Messaging.Core/Services/RabbitMQService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Configuration;
+using System.Text;
+using System.Threading.Tasks;
+using Messaging.Models;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Messaging.Core.Services
+{
+	public class RabbitMQService
+	{
+		private readonly string _hostName = ConfigurationManager.ConnectionStrings["RabbitMQHostname"].ConnectionString;
+		private readonly ConnectionFactory _factory;
+
+		public RabbitMQService()
+		{
+			_factory = new ConnectionFactory() { HostName = _hostName };
+		}
+
+		public void SendMessage(string message, string exchange, string routingKey = "", string type = "fanout")
+		{
+			using (var connection = _factory.CreateConnection())
+			using (var channel = connection.CreateModel())
+			{
+				if (!string.IsNullOrEmpty(exchange))
+					channel.ExchangeDeclare(exchange, type, false, false, null);
+
+				var properties = channel.CreateBasicProperties();
+				properties.Persistent = true;
+
+				var body = Encoding.UTF8.GetBytes(message);
+				channel.BasicPublish(exchange, routingKey, true, properties, body);
+			}
+		}
+
+		public GenericMessage ReadMessageAsync(string exchange, string queue, string routingKey = "", string type = "fanout")
+		{
+			GenericMessage message = null;
+
+			using (var connection = _factory.CreateConnection())
+			using (var channel = connection.CreateModel())
+			{
+				channel.ExchangeDeclare(exchange, type, false, false);
+				channel.QueueDeclare(queue, false, false, false, null);
+				channel.QueueBind(queue, exchange, routingKey);
+
+				var consumer = new EventingBasicConsumer(channel);
+				var result = channel.BasicGet(queue, true);
+
+				if (result != null)
+				{
+					message = new GenericMessage()
+					{
+						Body = Encoding.UTF8.GetString(result.Body),
+						MessageId = result.BasicProperties.MessageId
+					};
+				}
+			}
+
+			return message;
+		}
+	}
+}
---
+++ MessagingTests/Services/RabbitMQServiceTests.cs
@@ -0,0 +1,35 @@
+﻿using Messaging.Core.Services;
+using NUnit.Framework;
+
+namespace ServiceBusTests.Services
+{
+	public class RabbitMQServiceTests
+	{
+		private RabbitMQService _sut;
+
+		[SetUp]
+		public void Setup()
+		{
+			_sut = new RabbitMQService();
+		}
+
+		[Test]
+		public void SendMessages()
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				_sut.SendMessage("message", exchange: "messages");
+			}
+
+			Assert.IsTrue(true);
+		}
+
+		[Test]
+		public void ReadMessage()
+		{
+			var message = _sut.ReadMessageAsync("messages", "defaultqueue");
+			Assert.IsNotNull(message);
+			Assert.AreEqual(message.Body, "message");
+		}
+	}
+}
