5bf5e75623c22fb2839aa9868c1a49d3a12cdeb9
Mirko Maggioni <mirko.maggioni@sintel.net>
2018-05-12 12:53:11 +0200

    test methods for routing key are implemented

--- MessagingTests/Services/RabbitMQServiceTests.cs
+++ MessagingTests/Services/RabbitMQServiceTests.cs
@@ -12,8 +12,14 @@ namespace ServiceBusTests.Services
 	public class RabbitMQServiceTests
 	{
 		private RabbitMQService _sut;
-		private readonly string exchange = "messages";
-		private readonly string queue = "defaultqueue";
+		private readonly string sharedExchange = "message.shared";
+		private readonly string directExchange = "message.direct";
+		private readonly string sharedQueue = "message-shared-queue";
+		private readonly string consumerQueue = "message-consumer-queue";
+		private readonly string hrQueue = "message-hr-queue";
+		private readonly string hrConsumerQueue = "message-hr-consumer-queue";
+		private readonly string hrRoutingKey = "message.hr";
+		private readonly string marketingRoutingKey = "message.marketing";
 
 		[SetUp]
 		public void Setup()
@@ -24,20 +30,35 @@ namespace ServiceBusTests.Services
 		[Test]
 		public void MessagesAreSent()
 		{
-			for (int i = 0; i < 10; i++)
-			{
-				_sut.Publish("message", exchange: exchange);
-			}
-
+			PublishMessages("test message", sharedExchange);
 			Assert.IsTrue(true);
 		}
 
 		[Test]
 		public void AllMessagesInTheQueueAreReaded()
 		{
-			var messages = _sut.Get(exchange, queue);
+			PublishMessages("test message", sharedExchange);
+			var messages = _sut.Get(sharedExchange, sharedQueue);
 			Assert.IsNotNull(messages);
-			Assert.AreEqual(messages.ElementAt(0).Body, "message");
+			Assert.AreEqual(messages.ElementAt(0).Body, "test message");
+		}
+
+		[Test]
+		public void AllMessagesWithARoutingKeyAreReaded()
+		{
+			PublishMessages("hr message", directExchange, hrRoutingKey, "direct");
+			var messages = _sut.Get(directExchange, hrQueue, hrRoutingKey, "direct");
+			Assert.IsNotNull(messages);
+			Assert.AreEqual(messages.ElementAt(0).Body, "hr message");
+		}
+
+		[Test]
+		public void AllMessagesWithAnotherRoutingKeyAreNotReaded()
+		{
+			PublishMessages("marketing message", directExchange, marketingRoutingKey, "direct");
+			var messages = _sut.Get(directExchange, hrQueue, hrRoutingKey, "direct");
+			Assert.IsNotNull(messages);
+			Assert.AreEqual(messages.Count(), 0);
 		}
 
 		[Test]
@@ -51,27 +72,85 @@ namespace ServiceBusTests.Services
 
 			var publisherTask = Task.Run(() =>
 			{
-				for (int i = 0; i < 10; i++)
-				{
-					_sut.Publish("test message", exchange);
-				}
+				PublishMessages("test message", sharedExchange);
 			});
 
 			var cancellationTokenSource = new CancellationTokenSource();
 			var subscriberTask = Task.Run(() =>
 			{
-				_sut.SubscribeAsync(exchange, queue, callback, cancellationTokenSource).ConfigureAwait(false);
+				_sut.SubscribeAsync(sharedExchange, consumerQueue, callback, cancellationTokenSource).ConfigureAwait(false);
 			});
 
-			var cancelSubscriberTask = Task.Run(async () =>
+			Task.WaitAll(publisherTask, subscriberTask, CancelSubscriberTask(cancellationTokenSource));
+
+			Assert.AreEqual(messages.Count, 10);
+		}
+
+		[Test]
+		public void AllMessagesWithARoutingKeyAreConsumed()
+		{
+			var messages = new List<GenericMessage>();
+			var callback = new Action<GenericMessage>((message) =>
 			{
-				await Task.Delay(10000).ConfigureAwait(false);
+				messages.Add(message);
+			});
+
+			var publisherTask = Task.Run(() =>
+			{
+				PublishMessages("hr message", directExchange, hrRoutingKey, "direct");
+			});
+
+			var cancellationTokenSource = new CancellationTokenSource();
+			var subscriberTask = Task.Run(() =>
+			{
+				_sut.SubscribeAsync(directExchange, hrConsumerQueue, callback, cancellationTokenSource, hrRoutingKey, "direct").ConfigureAwait(false);
+			});
+
+			Task.WaitAll(publisherTask, subscriberTask, CancelSubscriberTask(cancellationTokenSource));
+
+			Assert.AreEqual(messages.Count, 10);
+		}
+
+		[Test]
+		public void AllMessagesWithAnotherRoutingKeyAreNotConsumed()
+		{
+			var messages = new List<GenericMessage>();
+			var callback = new Action<GenericMessage>((message) =>
+			{
+				messages.Add(message);
+			});
+
+			var publisherTask = Task.Run(() =>
+			{
+				PublishMessages("marketing message", directExchange, marketingRoutingKey, "direct");
+			});
+
+			var cancellationTokenSource = new CancellationTokenSource();
+			var subscriberTask = Task.Run(() =>
+			{
+				_sut.SubscribeAsync(sharedExchange, hrConsumerQueue, callback, cancellationTokenSource, hrRoutingKey, "direct").ConfigureAwait(false);
+			});
+
+			Task.WaitAll(publisherTask, subscriberTask, CancelSubscriberTask(cancellationTokenSource));
+
+			Assert.AreEqual(messages.Count, 0);
+		}
+
+		private void PublishMessages(string message, string exchange, string routingKey = "", string type = "fanout")
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				_sut.Publish(message, exchange, routingKey, type);
+			}
+		}
+
+		private Task CancelSubscriberTask(CancellationTokenSource cancellationTokenSource)
+		{
+			return Task.Run(async () =>
+			{
+				await Task.Delay(5000).ConfigureAwait(false);
 				cancellationTokenSource.Cancel();
 			});
-
-			Task.WaitAll(publisherTask, subscriberTask, cancelSubscriberTask);
-
-			Assert.That(messages.Count == 10);
 		}
 	}
 }
